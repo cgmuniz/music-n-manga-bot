460799144e37263e40e1e89d66a5254315c0ff68
cgmuniz <cauamuniz00@gmail.com>
2024-03-29 10:58:37 -0300

    correção horario intervalo

--- index.js
+++ index.js
@@ -230,7 +230,7 @@ function executarAcaoH(horario, acao) {
 
   setTimeout(() => {
       acao();
-      setInterval(acao, 12 * 60 * 60 * 1000);
+      setInterval(acao, 24 * 60 * 60 * 1000);
   }, tempoAteProximoHorario);
 }
 
