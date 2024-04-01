0b572900e2f910b11ac140c469dcca548ff9cc44
cgmuniz <cauamuniz00@gmail.com>
2024-04-01 01:15:32 -0300

    responder no chat em vez de dar reply

--- commands/risos.js
+++ commands/risos.js
@@ -11,7 +11,7 @@ module.exports = {
             return message.channel.send({ content: `<@${user.id}>`, files: ['./media/risos.mp4'] })
         }
 
-        return message.reply({ files: ['./media/risos.mp4'] })
+        return message.channel.send({ files: ['./media/risos.mp4'] })
     },
 }
 
