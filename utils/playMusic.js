212904b53a9c3fe2cc98ff6b112eba57dd881cbf
cgmuniz <cauamuniz00@gmail.com>
2024-04-08 00:30:23 -0300

    botMessage null

--- utils/playMusic.js
+++ utils/playMusic.js
@@ -15,8 +15,11 @@ module.exports = {
 
         serverQueue.player.play(songStream)
 
-        if (serverQueue.botMessage) await serverQueue.botMessage.delete(10000)
-
+        if (serverQueue.botMessage) {
+            await serverQueue.botMessage.delete(10000)
+            serverQueue.botMessage = null
+        }
+        
         serverQueue.textChannel.send(`Tocando: **${song.title}** ${song.duration}`).then(msg => serverQueue.botMessage = msg)
     },
 }
